35bc65e2451e8c826bb28317907d416b534ed9fd
Matthias Hauber <matthiashauber@googlemail.com>
2024-09-25 22:36:22 +0200

    binutils: add recipe for version 2.43.1

---
+++ recipes/binutils.lua
@@ -0,0 +1,14 @@
+pkg.name = "binutils"
+pkg.version = "2.43.1"
+
+pkg.scm = {
+  git = {{
+    remote = "git://sourceware.org/git/binutils-gdb.git",
+    rev = string.format("%s-%s_%s_%s", pkg.name, pkg.major, pkg.minor, pkg.patch)
+  }}
+}
+
+pkg.prepare = {
+  string.format("./configure --prefix=%s/image --with-sysroot=%s/sysroot --target=%s --disable-nls --enable-gprofng=no --disable-werror --enable-new-dtags --enable-default-hash-style=gnu",
+    W, W, target)
+}
